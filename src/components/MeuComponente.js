6db8ce72376c498fde183d5d561a7f64e3aaf9bc
Gabriel-Arcenio <ga.vas.silva@gmail.com>
2025-08-20 12:07:19 -0300

    Add files via upload

---
+++ src/components/MeuComponente.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+//export default function MeuComponente() {
+//  return <Text>Este é um componente reutilizável!</Text>;
+//}
+const MeuComponente = () => {
+  return <Text> componente funcional!</Text>;
+}; 
+export default MeuComponente;
---
+++ src/screens/inicio.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      {/* Imagem */}
+      <Image 
+        source={require('./assets/casual_dog.png')} 
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      {/* Título */}
+      <Text style={styles.title}>Ótimo dia!</Text>
+      <Text style={styles.subtitle}>Como deseja acessar?</Text>
+
+{/* Botão Google */}
+<TouchableOpacity style={styles.googleButton}>
+  <Image 
+    source={require('./assets/Google.png')} // coloque o ícone do Google na pasta assets
+    style={styles.googleIcon}
+  />
+  <Text style={styles.googleButtonText}>Como deseja acessar?</Text>
+</TouchableOpacity>
+
+
+      {/* Botão Outras opções */}
+      <TouchableOpacity style={styles.outrosButton}>
+        <Text style={styles.outrosButtonText}>Outras opções</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  image: {
+    width: 250,
+    height: 250,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#555',
+    marginBottom: 20,
+  },
+  googleButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#34A853',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  googleIcon: {
+  width: 20,
+  height: 20,
+  marginRight: 10,
+},
+  googleButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  outrosButton: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  outrosButtonText: {
+    fontSize: 16,
+    color: '#333',
+  },
+});
